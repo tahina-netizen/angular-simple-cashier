67fd713ebdb50e3b7ce81d4a0c47314cc9f77eb1
Zo Tahina <zratefiarivony@gmail.com>
2020-08-05 17:14:43 +0200

    feat(): "message" service
    ...

---
+++ src/app/message.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  message: string[] = [];
+
+  constructor() { }
+
+  add(message: string): void {
+    this.message.push(message);
+  }
+  
+  clear(): void {
+    this.message = [];
+  }
+}
