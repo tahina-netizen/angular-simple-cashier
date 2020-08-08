f15d72caa77596c1e2e5cfd3725d0b12b5512691
Zo Tahina <zratefiarivony@gmail.com>
2020-08-08 18:43:30 +0200

    fix(): fixing data in database
    adding value to "price" property of products

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -12,16 +12,16 @@ export class InMemoryDataService implements InMemoryDbService{
 
   createDb() {
     const products = [
-      { id: 11, name: 'Fruit', quantity: 1 },
-      { id: 12, name: 'Beer' , quantity: 1 },
-      { id: 13, name: 'Bread', quantity: 1 },
-      { id: 14, name: 'Soda', quantity: 1 },
-      { id: 15, name: 'Rice', quantity: 1 },
-      { id: 16, name: 'Water', quantity: 1 },
-      { id: 17, name: 'Cheese', quantity: 1 },
-      { id: 18, name: 'Book', quantity: 1 },
-      { id: 19, name: 'Soap', quantity: 1 },
-      { id: 20, name: 'Mask', quantity: 1 }
+      { id: 11, name: 'Fruit', quantity: 1, price: 5 },
+      { id: 12, name: 'Beer' , quantity: 1, price: 10 },
+      { id: 13, name: 'Bread', quantity: 1, price: 3 },
+      { id: 14, name: 'Soda', quantity: 1, price: 5 },
+      { id: 15, name: 'Rice', quantity: 1, price: 20 },
+      { id: 16, name: 'Water', quantity: 1, price: 2 },
+      { id: 17, name: 'Cheese', quantity: 1, price: 9 },
+      { id: 18, name: 'Book', quantity: 1, price: 20 },
+      { id: 19, name: 'Soap', quantity: 1, price: 5 },
+      { id: 20, name: 'Mask', quantity: 1, price: 25 }
     ];
     return {products};
   }
