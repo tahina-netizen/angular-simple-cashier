6afa90fe8d4abd6b61557bda36ecca774bdb29f7
Zo Tahina <zratefiarivony@gmail.com>
2020-08-10 10:41:49 +0200

    feat(): routing and routerLinks
    ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProductsComponent } from './products/products.component'
+import { MarketComponent } from './market/market.component';
+import { CartComponent } from './cart/cart.component';
 
 
 const routes: Routes = [
-  {path: 'products', component: ProductsComponent}
+  { path: '', redirectTo: '/market', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent },
+  { path: 'market', component: MarketComponent },
+  { path: 'cart', component: CartComponent },
 ];
 
 @NgModule({
